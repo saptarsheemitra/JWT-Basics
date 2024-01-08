ca19840d1c512c0875e275158c11bbf27f2bd0e1
Saptarshee Mitra <saptarshee@one800.help>
2024-01-08 17:32:57 +0530

    added routes

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,25 @@
+require('express-async-errors');
+const jwt = require('jsonwebtoken')
 const testEndpoint = (req, res) => {
     res.send("Hello World");
 }
 
-module.exports = { testEndpoint }
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(401).send("Username/Password not provided");
+        // throw new Error("Username/Password not provided");
+
+    }
+    const token = jwt.sign({})
+
+    res.send({ status: 'success', data: { username, password } });
+
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.send({ msg: `Hello Sap`, secret: `Here's ur number ${luckyNumber}` });
+}
+
+module.exports = { testEndpoint, login, dashboard }
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { testEndpoint } = require('../controllers/main')
+const { testEndpoint, login, dashboard } = require('../controllers/main')
 
 router.get('/', testEndpoint)
+router.get('/dashboard', dashboard);
+router.post("/login", login)
 
 module.exports = router;
